a26adf004624af48ac244920e18eb839dc33391a
Yousef Obeidat <y.linux96@gmail.com>
2021-12-21 13:41:49 +0200

    lab-38

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(props) {
+  return (
+    <footer className="flex flex-col justify-center w-full py-3 pl-3 bg-green-500">
+      <p > {props.len} Location World Wide</p>
+    </footer>
+  )
+}
